2718c94d9f410bfee7e4d7c5a8e03e64e4f37dd0
Dhriti11 <65971188+Dhriti11@users.noreply.github.com>
2020-11-01 15:48:16 +0530

    Database for Vitality Green
    This is the initial database for Vitality Green

---
+++ VITALITY GREEN.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2020 at 01:03 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vitality_green`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carbon_record`
+--
+
+CREATE TABLE `carbon_record` (
+  `id` varchar(20) DEFAULT NULL,
+  `vehicle_count` bigint(20) DEFAULT NULL,
+  `carbon_footprint` bigint(20) DEFAULT NULL,
+  `state` char(30) DEFAULT NULL,
+  `carbon_footprint_proof` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
